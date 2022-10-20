239a0fa2eb7751612d1698f896a51527b466b054
Júlia Rosa <julia.britorosa@gmail.com>
2022-10-20 12:17:42 -0300

    Commit da primeira questão

---
+++ prova_algoritmo.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	printf(" Disciplina: Algoritmo e Programacao \n Professora: Joyce Siqueira \n Aluno: Julia Cerqueira Brito Rosa \n Matricula: UC22200243 Curso: Analise e Desenvolvimento de Sistemas \n Repositorio: https://github.com/rosa-julia/Prova_Algoritmo \n Dev C++");
+	
+	
+}
+
+
